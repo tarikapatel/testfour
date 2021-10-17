86934112ef28c83da10b74d75b1c1ddce876bbdb
tarikapatel <tarikapatel23@gmail.com>
2021-10-17 17:15:51 +0100

    test4print

---
+++ src/TestFour.java
@@ -0,0 +1,11 @@
+public class TestFour
+{
+    public static void main (String args[])
+    {
+        System.out.println("I like Git.");
+        System.out.println("I like Git.");
+        System.out.println("I like Git.");
+        System.out.println("I like Git.");
+        
+    }
+}
